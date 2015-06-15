c70b533821d39d72f6fb081845b15d74f938d4f7
Andrew Hanes <ahanes@csh.rit.edu>
2015-06-14 20:16:08 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='si_middle',
+    version='1.0.0',
+    packages=['simiddle', 'simiddle.migrations', 'si_middle'],
+    url='',
+    license='MIT',
+    author='ahanes',
+    author_email='ahanes@csh.rit.edu',
+    description='middleware for si app'
+)
